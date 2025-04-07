3db4335c5502404ff8352c08be38a8acbafc3d1b
tt619252615 <619252615@qq.com>
2025-04-07 17:01:29 +0800

    Update nixpkgs reference to version 24.11 in flake.lock and flake.nix.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "The toolkits of devops.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
